75a4425bc02809a2c40701eb814a19c78e483fef
hans <mirojburhanudin2@gmail.com>
2024-11-01 18:39:14 +0700

    Ganti title tabel produk

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,7 +13,7 @@
         <div class="row">
             <div class="col-md-12">
                 <div>
-                    <h3 class="text-center my-4">Tutorial Laravel 11 untuk Pemula</h3>
+                    <h3 class="text-center my-4">Tabel Produk</h3>
                     <h5 class="text-center"><a href="https://naufalriski.com">www.naufalriski.com</a></h5>
                     <hr>
                 </div>
